68f6407ff7c1b954ceb169f966f9adc98bfc4cf6
wh0syx <wh0syx@proton.me>
2025-05-12 12:35:32 +0200

    Integrated API with products

--- src/components/PopularItems/PopularItems.jsx
+++ src/components/PopularItems/PopularItems.jsx
@@ -1,18 +1,31 @@
-import React from "react"
-import styles from "./PopularItems.module.css"
+import React, { useEffect, useState } from "react";
+import styles from "./PopularItems.module.css";
 
-function PopularItems(props) {
+function PopularItems() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://reactapplicationapi.onrender.com/products/")
+            .then((res) => {
+                if (!res.ok) throw new Error("Failed to fetch products");
+                return res.json();
+            })
+            .then(setProducts)
+            .catch((err) => console.error("Error loading products:", err));
+    }, []);
+
     return (
         <div className={styles.PopularItems}>
             <h1 className={styles.Title}>Popular products</h1>
             <div className={styles.ProductGrid}>
-                <div className={styles.ProductImage}></div>
-                <div className={styles.ProductImage}></div>
-                <div className={styles.ProductImage}></div>
-                <div className={styles.ProductImage}></div>
+                {products.map((product) => (
+                    <div key={product.name} className={styles.ProductImage}>
+                        [{product.name}]
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default PopularItems
+export default PopularItems;
